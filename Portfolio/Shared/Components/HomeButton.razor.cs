23dba9ffb6943fe49384370596b15f1efcefc0b9
Nelsocol <collinnelson509@gmail.com>
2022-09-15 16:17:34 -0700

    Outlined basic project page example

---
+++ Portfolio/Shared/Components/HomeButton.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Shared.Components
+{
+    public partial class HomeButton
+    {
+        [Inject]
+        protected NavigationManager NavManager { get; set; }
+
+        protected void RedirectHome()
+        {
+            NavManager.NavigateTo("/");
+        }
+    }
+}
---
+++ Portfolio/Shared/Components/ProjectCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Shared.Components
+{
+    public partial class ProjectCard
+    {
+        [Inject]
+        protected NavigationManager NavManager { get; set; }
+
+        protected void RedirectToProject() 
+        {
+            NavManager.NavigateTo("/outlineProject");
+        }
+    }
+}
